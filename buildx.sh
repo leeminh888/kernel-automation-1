008a49267606a1a5e8e6c929f84bc40bf9de27f4
leeminh888 <68751029+leeminh888@users.noreply.github.com>
2021-04-22 09:15:16 +0700

    Create buildx.sh

---
+++ buildx.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/leeminh888/kernel_xiaomi_vayu -b r --depth 1
+cd /home/runner/work/kernel-automation-1/kernel-automation-1/kernel_xiaomi_vayu
+chmod +x build.sh && ./build.sh
